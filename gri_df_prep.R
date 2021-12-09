f3089a46affc4068a08da2523a341f7fe3427abb
ia-nechaev <ivann@business.aau.dk>
2021-12-09 10:47:01 +0000

    Working dwnlding script

--- gri_df_prep.R
+++ gri_df_prep.R
@@ -24,12 +24,12 @@ gri_rep_df <- gri_rep_df %>%
 gri_rep_df %>% filter(!Report_PDF_Address=="") %>% count
 
 # make the list of pdf links to download
-url_list <- gri_rep_df %>% filter(!Report_PDF_Address=="")
+url_list <- gri_rep_df %>% filter(!Report_PDF_Address=="") %>% filter(!is.na(Organization))
 
 # make the html report list
-rem_pdf <- regex("(\\.pdf)", ignore_case = TRUE )
-hurl_list <- gri_rep_df %>% filter(!Report_HTML_Address=="", !str_detect(Report_HTML_Address,rem_pdf))
-hurl_list %<>% mutate(Report_Title=iconv(Report_Title,from="UTF-8", to="ASCII//TRANSLIT"))
+#rem_pdf <- regex("(\\.pdf)", ignore_case = TRUE )
+#hurl_list <- gri_rep_df %>% filter(!Report_HTML_Address=="", !str_detect(Report_HTML_Address,rem_pdf))
+#hurl_list %<>% mutate(Report_Title=iconv(Report_Title,from="UTF-8", to="ASCII//TRANSLIT"))
 
 # remove p. page pages in pdf contents column
 rem_letters <- regex("(?:p\\. |page.? |p )", ignore_case = TRUE )
@@ -37,6 +37,7 @@ rem_letters <- regex("(?:p\\. |page.? |p )", ignore_case = TRUE )
 url_list <- url_list %>% mutate(pdf_contents_page=str_remove(Content_index_location_PDF, rem_letters))
 url_list <-url_list %>% mutate(Report_Title=iconv(Report_Title,from="UTF-8", to="ASCII//TRANSLIT"))
 
+url_list$Organization_Legal_Name %>% is.na() %>% sum()
 
 getwd()
 saving_dir <- "reports/"
@@ -46,86 +47,106 @@ print(str_c("Saving to ", path_to_save))
 # saving reports in a company name folder 
 
 # making a test list of urls and paths
-ran_gen_id <- floor(runif(30, min=1, max=3443))
+#ran_gen_id <- floor(runif(30, min=1, max=3443))
 #ran_gen_id <- c(ran_gen_id,3168)
 # temp_url_list <- url_list %>% slice(ran_gen_id) #dplyr solution
-temp_url_list <- url_list[ran_gen_id,]
+# temp_url_list <- url_list[ran_gen_id,]
+
 
 rem_punct <- regex("[[:punct:]]")
 
 # making folder and filename columns
-temp_url_list %<>% mutate(
+url_list %<>% mutate(
   folder=str_replace_all(
-            str_sub(
-              str_to_lower(
-                str_remove_all(Organization, rem_punct), locale = "en"),
-            start=1, end=15),
+            str_squish(
+              str_sub(
+                str_to_lower(
+                  str_remove_all(Organization, rem_punct), locale = "en"),
+              start=1, end=20)
+            ),
           " ", "_"))
-temp_url_list %<>% mutate(
+
+url_list %<>% mutate(
+  ffolder=str_c(folder,"/",Publication_Year))
+
+url_list %<>% mutate(Report_Title=ifelse(is.na(Report_Title),url_list$Organization,Report_Title))
+
+#url_list$Report_Title %>% is.na() %>% sum() 
+
+url_list %<>% mutate(
   filename=str_replace_all(
-    str_sub(
-      str_to_lower(
-        str_remove_all(Report_Title, rem_punct), locale = "en"),
-    ),
-    " ", "_") %>% str_c(".pdf"))
+            str_squish(
+              str_sub(
+                str_to_lower(
+                  str_remove_all(Report_Title, rem_punct), locale = "en"),
+              )
+            ),
+            " ", "_") %>% str_c(".pdf"))
 
-temp_url_list %<>% mutate(request_status="", type_dld="", url_dld="") 
+
+temp_url_list <- url_list[1:3432,]
+temp_url_list %<>% mutate(request_status="", type_dld="", url_dld="", dld_status="") 
+# setwd(path_to_save)
+# dir.create(temp_url_list$ffolder[2], recursive = T)
+# temp_url_list$folder[1]
+# temp_url_list$Report_PDF_Address %>% is.na() %>% sum()
+# temp_url_list <- temp_url_list[!(is.na(temp_url_list$Organization)),]
+
+create_directories()
+
+# cat(req$status_code," ", req$type,  "\n")
+
+curl_options(filter="alive")
+curl_symbols("error")
+
+#download all reports (var 2)
 setwd(path_to_save)
-temp_url_list$folder[1]
+h <-new_handle(failonerror = F, ssl_verifyhost = F, ssl_verifypeer = F)
+handle_setopt(h, use_ssl=1)
+# handle_setopt(h, tcp_keepalive=F)
 
-for(i in 1:nrow(temp_url_list)){
-  #create company dir
-  setwd(path_to_save)
-  if (file.exists(temp_url_list$folder[i])) {
-    cat("The folder ", temp_url_list$folder[i], " already exists")
-  } else {
-  
-    dir.create(temp_url_list$folder[i])
-    setwd(str_c(path_to_save,temp_url_list$folder[i]))
-    
-    #create year directory
-    if (file.exists(toString(temp_url_list$Publication_Year[i]))) {
-      cat("The folder ", temp_url_list$Publication_Year[i], " already exists")
-    } else {
-      dir.create(toString(temp_url_list$Publication_Year[i]))
-    }
-  }
-}
-
-#download all reports var 1
-#setwd(path_to_save)
-#for(i in 1:nrow(temp_url_list)){
-#  download.file(temp_url_list$Report_PDF_Address[i],
-#                str_c(temp_url_list$folder[i],
-#                      temp_url_list$Publication_Year[i],
-#                      temp_url_list$filename[i], sep = "/"))
-#}
-
-#download all reports var 2
-setwd(path_to_save)
-h <-new_handle(failonerror = T)
 i<-1
+j<-1
 
 while(i<=nrow(temp_url_list)){
- 
   out <- tryCatch(
     expr = {
-     req <- curl_fetch_disk(temp_url_list$Report_PDF_Address[i],
-                      str_c(temp_url_list$folder[i],
-                            temp_url_list$Publication_Year[i],
-                            temp_url_list$filename[i], sep = "/"),
-                      handle = h
-            )
+      my_statement1 <-!(file.exists(str_c(temp_url_list$ffolder[i], temp_url_list$filename[i], sep = "/")))
+      if(my_statement1){
+        req <- curlGetHeaders(temp_url_list$Report_PDF_Address[i], redirect = TRUE, verify = FALSE)
+        temp_url_list$request_status[i] <- attr(req, "status")
+        # close.connection(req)
+        my_statement <- (attr(req, "status")==200)
+        if (my_statement){
+          curl_download(temp_url_list$Report_PDF_Address[i],
+                          str_c(temp_url_list$ffolder[i],
+                                #temp_url_list$Publication_Year[i],
+                                temp_url_list$filename[i], sep = "/"),
+                          handle = h,
+                          quiet = T)
+          temp_url_list$dld_status[i] = "OK"
+          #cat(req$status_code," ", req$type,  "\n")
+          
+        } else{
+          temp_url_list$dld_status[i] = "ERR 1"
+        }
+        #closeAllConnections()
+      }else{
+        temp_url_list$dld_status[i] = "FE"
+      }
+      #cat(req$status_code," ", req$type,  "\n")
       
-      cat(req$status_code," ", req$type,  "\n")
       
-      temp_url_list$request_status[i] <- req$status_code
-      temp_url_list$url_dld[i]        <- req$url
-      temp_url_list$type_dld[i]       <- req$type
+      #temp_url_list$url_dld[i]        <- req$url
+      #temp_url_list$type_dld[i]       <- req$type
+      cat(temp_url_list$ffolder[i], temp_url_list$request_status[i], temp_url_list$dld_status[i], "\n", sep = "  ")
+      
+
       
     },
     error = function(e){ 
-      temp_url_list$request_status[i] <- "Error dw"
+      temp_url_list$request_status[i] <- e
+      temp_url_list$dld_status[i] = "ERR 2"
       cat("Error: ", temp_url_list$Organization[i], req$status_code," ", req$type,  "\n")
       return(NULL)
     },
@@ -138,11 +159,34 @@ while(i<=nrow(temp_url_list)){
       # Do this at the end before quitting the tryCatch structure...
     }
   )
-  i=i+1
+  
+  if(j==200){
+    write.csv(temp_url_list, "GriTempUrlList3.csv")
+    gc()
+    j<-0
+  }
+  
+  i<-i+1
+  j<-j+1
+
 }
 
 
-
+create_directories <- function(){
+  
+  for(i in 1:nrow(temp_url_list)){
+    #create company and year dirs
+    setwd(path_to_save)
+    if (file.exists(temp_url_list$ffolder[i])) {
+      cat("The folder ", temp_url_list$ffolder[i], " already exists", "\n")
+      
+    } else {
+      dir.create(temp_url_list$ffolder[i], recursive = T)
+    }
+    
+  }
+  
+}
 
 write.csv(temp_url_list, "GriTempUrlList2.csv")
 
@@ -151,7 +195,10 @@ write.csv(hurl_list, "GriUrlList.csv")
 
 
 
-#_______________________________
+# _______________________________ DRAFTS __________________________
+
+
+
 tmpf <-tempfile()
 
 curl_download("https://vp292.alertir.com/afw/files/press/handelsbanken/202002135907-1.pdf", dir)
@@ -206,3 +253,57 @@ out <- tryCatch(
     return(NULL)
   }
 )  
+
+
+### old create directory procedure (faulty)
+
+
+for(i in 1:nrow(temp_url_list)){
+  #create company dir
+  setwd(path_to_save)
+  if (file.exists(temp_url_list$ffolder[i])) {
+    cat("The folder ", temp_url_list$ffolder[i], " already exists")
+    
+    #create year directory
+    if (file.exists(toString(temp_url_list$Publication_Year[i]))) {
+      cat("The folder ", temp_url_list$Publication_Year[i], " already exists")
+    } else {
+      dir.create(toString(temp_url_list$Publication_Year[i]))
+    }
+  } else {
+    
+    dir.create(temp_url_list$folder[i])
+    setwd(str_c(path_to_save,temp_url_list$folder[i]))
+    
+    #create year directory
+    if (file.exists(toString(temp_url_list$Publication_Year[i]))) {
+      cat("The folder ", temp_url_list$Publication_Year[i], " already exists")
+    } else {
+      dir.create(toString(temp_url_list$Publication_Year[i]))
+    }
+  }
+}
+
+while(i<=nrow(temp_url_list)){
+  curl_download(temp_url_list$Report_PDF_Address[i],
+                str_c(temp_url_list$ffolder[i],
+                      temp_url_list$filename[i], sep = "/"),
+                handle = h,
+                quiet = F
+  )
+  i<-i+1
+}
+
+curl("https://httpbin.org/status/418", "r")
+
+req <- curlGetHeaders(temp_url_list$Report_PDF_Address[10], redirect = TRUE, verify = FALSE)
+attr(req, "status")
+
+#download all reports var 1
+#setwd(path_to_save)
+#for(i in 1:nrow(temp_url_list)){
+#  download.file(temp_url_list$Report_PDF_Address[i],
+#                str_c(temp_url_list$folder[i],
+#                      temp_url_list$Publication_Year[i],
+#                      temp_url_list$filename[i], sep = "/"))
+#}
